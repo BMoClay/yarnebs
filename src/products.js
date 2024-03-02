f9d41e722ae6c834d22e1a4715611dcd10fc98e1
BMoClay <benmorgancleveland@gmail.com>
2024-03-01 19:19:15 -0500

    basic setup 23:40

---
+++ src/products.js
@@ -0,0 +1,54 @@
+import product1 from './assets/IMG_9262 copy.jpeg';
+import product2 from './assets/IMG_9266 copy.jpeg';
+import product3 from './assets/IMG_9267 copy.jpeg';
+import product4 from './assets/IMG_9268 copy.jpeg';
+import product5 from './assets/IMG_9269 copy.jpeg';
+import product6 from './assets/IMG_9270 copy.jpeg';
+import product7 from './assets/IMG_9275 copy.jpeg';
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "IPhone, 2023",
+        price: 999.0,
+        productImage: product1,
+    },
+    {
+        id: 1,
+        productName: "Bolo, 2024",
+        price: 999.0,
+        productImage: product2,
+    },
+    {
+        id: 1,
+        productName: "Shoe Book",
+        price: 999.0,
+        productImage: product3,
+    },
+    {
+        id: 1,
+        productName: "Dog-eared",
+        price: 999.0,
+        productImage: product4,
+    },
+    {
+        id: 1,
+        productName: "Obtuse reader",
+        price: 999.0,
+        productImage: product5,
+    },
+    {
+        id: 1,
+        productName: "Style tips",
+        price: 999.0,
+        productImage: product6,
+    },
+    {
+        id: 1,
+        productName: "IPhone repair",
+        price: 999.0,
+        productImage: product7,
+    }
+
+]
